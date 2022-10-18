85c365156bc6b48cfed9056e0d493874ab13e0be
RollerCoaster22 <73495757+RollerCoaster22@users.noreply.github.com>
2022-10-18 17:19:43 +0200

    Update comandaDAO.java

--- comandaDAO.java
+++ comandaDAO.java
@@ -10,53 +10,17 @@ import java.sql.SQLException;
 
 
 public class comandaDAO {
-    private static String countByUserName = "SELECT count(*) cuenta FROM users WHERE username = ?";
-    private static String findByUserName = "SELECT * FROM users WHERE username = ?";
-    private static String updateDate = "UPDATE users set last_login = current_timestamp where username = ?";
- 
-    public boolean validateUser(comandaVO user) {
-        boolean result = false;
-        try {
-        // Abrimos la conexión e inicializamos los parámetros
-        Connection conn = ConnectionManager.getConnection();
-
-        PreparedStatement countPs = conn.prepareStatement(countByUserName);
-        PreparedStatement findPs = conn.prepareStatement(findByUserName);
-        PreparedStatement updatePs = conn.prepareStatement(updateDate);
-        countPs.setString(1, user.getUserName());
-        findPs.setString(1, user.getUserName());
-        updatePs.setString(1, user.getUserName());
-        // Ejecutamos la consulta
-        ResultSet findRs = findPs.executeQuery();
-        ResultSet countRs = countPs.executeQuery();
-        // Leemos cuántos registros tenemos
-        countRs.next();
-        int n = countRs.getInt(1);
-        System.out.println("Número de registros: " + n);
-        // Si solo hay un resultado, la query es correcta
-        if(n == 1) {
-            // Comparamos contraseñas
-            findRs.next();
-            String dbpwd = findRs.getString("password");
-            if (dbpwd.contentEquals(user.getPassword())) {
-                updatePs.execute();
-                result = true;
-            }
-        }
-        // liberamos los recursos utilizados
-        findRs.close();
-        findPs.close();
-        countRs.close();
-        countPs.close();
-        updatePs.close();
-        PoolConnectionManager.releaseConnection(conn);
-        } 
-        catch(SQLException se) {
-            se.printStackTrace();
-        } 
-        catch(Exception e) {
-            e.printStackTrace(System.err);
-        }
-        return result;
+    List<comandaVO> comandas;
+    public platoDAO(){
+        comandas=new ArrayList<comandaVO>();
+    }
+    public void anyadirComanda(comandaVO c){
+        comandas.add(c);
+    }
+    public void eliminarComanda(comandaVO c){
+        comandas.remove(p.obtenerIdComanda);
+    }
+    public comandaVO obtenerComanda(int idComanda){
+        return(comandas.get(idComanda));
     }
 }
