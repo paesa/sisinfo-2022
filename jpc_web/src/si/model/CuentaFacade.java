ef2b2ad222e68823fdf923758a32ba626559c3e3
carlos <carlospaesa@gmail.com>
2022-11-01 18:19:09 +0100

    Update CuentaFacade.java
    Co-Authored-By: RollerCoaster22 <73495757+RollerCoaster22@users.noreply.github.com>
    Co-Authored-By: pablobueno01 <74594574+pablobueno01@users.noreply.github.com>

--- jpc_web/src/si/model/CuentaFacade.java
+++ jpc_web/src/si/model/CuentaFacade.java
@@ -20,12 +20,61 @@ public class CuentaFacade {
     }
 
     public CuentaVO registrarCuenta(String apodo, String correo, String contrasegna) throws CorreoYaRegistrado  {
-        CuentaVO cuentaVO = new CuentaVO()
+        CuentaVO cuentaVO = new CuentaVO(,correo,apodo,contrasegna,) //id random y tipo cuenta
+        CuentaDAO userDAO = (CuentaDAO) new CuentaDAO();
+		Connection connection =null;
+		try {	
+            connection = this.dataSource.getConnection();
+            if (connection == null) System.out.println("Conexión es: null");
+            if (connection != null) System.out.println("Conexión es: not null "+ connection);
+            if (CuentaDAO.exist(connection, CuentaVO.obtenerCorreo())) {
+                throw new CorreoYaRegistrado();
+            }else{
+                CuentaDAO.create(connection, CuentaVO);
+            } 
+		} catch (SQLException e) {
+			throw new InternalErrorException(e);
+		} finally {
+			try {
+				if (connection !=null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				throw new InternalErrorException(e);
+			}
+		}
         return null;
     }
 
     public CuentaVO iniciarCuenta(String correo, String contrasegna) throws CorreoInvalido, ContrasegnaInvalida {
-        
+        CuentaDAO userDAO = (CuentaDAO) new CuentaDAO();
+		Connection connection =null;
+		CuentaVO result = null;
+		try {
+			connection = this.dataSource.getConnection();
+			if (CuentaDAO.exist(connection, correo)) {
+                result = CuentaDAO.find(connection, correo);
+				if (result.contrasegna != constrasegna) {
+                    result = null;
+                    System.out.println("La contraseña es incorrecta");
+				    throw new ContrasegnaInvalida();		
+                }
+			}else {
+				System.out.println("No es usuario del sistema");
+				throw new CorreoInvalido();				
+			}
+			return result;
+		}catch (SQLException e) {
+			throw new InternalErrorException(e);
+		}finally {
+			try {
+				if (connection !=null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				throw new InternalErrorException(e);
+			}
+		}
         return null;
     }
 }
